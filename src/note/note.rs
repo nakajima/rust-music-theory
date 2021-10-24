538a2c425d119c69d1700831a7fd418f01ae8a81
nik <nik@parkellipsen.de>
2021-10-24 19:36:30 +0200

    add simple string and frequency conversion

--- src/note/note.rs
+++ src/note/note.rs
@@ -1,6 +1,7 @@
 use alloc::vec::Vec;
 
 use crate::note::Pitch;
+use crate::note::Tuning;
 
 /// A note.
 #[derive(Debug, Clone)]
@@ -13,8 +14,37 @@ pub struct Note {
 
 impl Note {
     /// Create a new note.
-    pub fn new(pitch: Pitch, octave: u8) -> Self {
+
+    pub fn new(pitch: Pitch, octave: u8) -> Note {
         Note { pitch, octave }
+    }
+
+    pub fn from_note_nr(nr: u8) -> Note {
+        let pitch_class = Pitch::from_u8(nr % 12);
+        let octave = nr / 12;
+        Note::new(pitch_class, octave)
+    }
+
+    pub fn from_freq(freq: f32, tuning: Tuning) -> Note {
+        match tuning {
+            Tuning::EqualTemperament => {
+                let a440 = Note::new(Pitch::from_str("A").unwrap(), 4).to_note_nr();
+                Note::from_note_nr(((12.0 * (freq / 440.0).log2()) as i16 + a440 as i16) as u8)
+            }
+        }
+    }
+
+    pub fn to_note_nr(self) -> u8 {
+        self.pitch.into_u8() + 12 * self.octave
+    }
+
+    pub fn to_freq(self, tuning: Tuning) -> f32 {
+        match tuning {
+            Tuning::EqualTemperament => {
+                let a440 = Note::new(Pitch::from_str("A").unwrap(), 4).to_note_nr();
+                2f32.powf(((self.to_note_nr() as i16 - a440 as i16) as f32) / 12.0) * 440.0
+            }
+        }
     }
 }
 
